bd97b3edb02ad924f1f47aa8b56267664584449e
kovtunsergey <flashback2018@mail.ru>
2018-06-09 22:26:55 +0700

    develop menu (test version)

---
+++ src/menu.cpp
@@ -0,0 +1,22 @@
+int menu()
+{
+	char *p;
+	p = "pic.jpg";
+	readimagefile(p,0,0,600,600);
+	int x2;
+	int y2;
+	while(1)
+	{
+		if(kbhit()) 
+		return // - code
+		if(ismouseclick(WM_LBUTTONDOWN))
+		{
+			getmouseclick(WM_LBUTTONDOWN,x2,y2);
+			if(x2 ... y2) //coord knopok menu
+			{
+				// return code ->>>>> main.cpp
+			 } 
+		}
+	}
+	return 0;
+}
