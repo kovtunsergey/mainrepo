13a661ef0bf094bc54623a0d79da366eeedec1db
kovtunsergey <flashback2018@mail.ru>
2018-06-09 21:37:38 +0700

    create func check win player?

---
+++ src/checkw.cpp
@@ -0,0 +1,12 @@
+bool checkw(const int (*massive)[4], int (*mass)[4])
+{
+	for(int i = 0; i < 4; i++)
+	{
+		for(int j = 0; j < 4; j++)
+		{
+			if(mass[i][j] != massive[i][j])
+			return 0;
+		}			
+	}
+	return 1;
+}
