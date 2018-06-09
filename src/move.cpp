d932ca7d009e18c2f7cc0c52f9773e9b349ed5b4
kovtunsergey <flashback2018@mail.ru>
2018-06-09 21:47:35 +0700

    check step, perfomance step,check exit

---
+++ src/move.cpp
@@ -0,0 +1,35 @@
+bool move(int (*mass)[4],int &x, int &y)
+{
+	int x1 = x;
+	int y1 = y;
+	char click;
+	printmass(mass);
+	while(1)	
+	{
+		if(kbhit() )
+		{
+			click = getch();
+			if(click == 27) 
+			return 1;
+			switch(click)
+			{
+				case 75: x1 = x; y1 = y - 1; // left
+				break;
+				case 77: x1 = x; y1 = y + 1; // right
+				break; 
+				case 72: x1 = x - 1; y1 = y; // up
+				break;
+				case 80: x1 = x + 1; y1 = y: // down
+				break;
+			}
+			break;
+		}
+	if(ismouseclick(WM_LBUTTONDOWN))
+	{
+		getmouseclick(WM_LBUTTONDOWN, y1, x1);
+		if((x1 < 25) && (y1 < 600) && (x1 > 0) && (y1 > 575)) 
+		return 1;
+		if((x1 < 90)||(y1 < 90))
+		continue;
+		x1 = (x1 - 90) / 100;
+		y1 = (y1 - 90) / 100;
