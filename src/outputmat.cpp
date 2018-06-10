d3cd40e023f1bbf9dedf49d4bd41e14d9dc7fc14
kovtunsergey <flashback2018@mail.ru>
2018-06-10 19:05:02 +0700

    output matrix

---
+++ src/outputmat.cpp
@@ -0,0 +1,46 @@
+void outputmat(int (*mass)[4], char *p0, char *p1, char *p2, char *p3, char *p4, char *p5,
+char *p6, char *p7, char *p8, char *p9, char *p10, char *p11, char *p12, char *p13,
+char *p14, char *p15)
+{
+	for(int i = 0; i < 4; i++)
+	{
+		for(int j = 0; j < 4; j++)
+		{
+			switch(mass[i][j])
+			{
+				case 0: readimagefile(p0, ..)
+				break;
+				case 1: readimagefile(p0, ..)
+				break;
+				case 2: readimagefile(p0, ..)
+				break;
+				case 3: readimagefile(p0, ..)
+				break;
+				case 4: readimagefile(p0, ..)
+				break;
+				case 5: readimagefile(p0, ..)
+				break;
+				case 6: readimagefile(p0, ..)
+				break;
+				case 7: readimagefile(p0, ..)
+				break;
+				case 8: readimagefile(p0, ..)
+				break;
+				case 9: readimagefile(p0, ..)
+				break;
+				case 10: readimagefile(p0, ..)
+				break;
+				case 11: readimagefile(p0, ..)
+				break;
+				case 12: readimagefile(p0, ..)
+				break;
+				case 13: readimagefile(p0, ..)
+				break;
+				case 14: readimagefile(p0, ..)
+				break;
+				case 15: readimagefile(p0, ..)
+				break;
+			}
+		}
+	}
+}
