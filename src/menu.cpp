2c14fc29b469f0237286d2c073ce268458cb3a59
kovtunsergey <flashback2018@mail.ru>
2018-06-12 15:34:06 +0700

    test coordinat's menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,18 +1,18 @@
 int menu()
 {
 	char *p;
-	p = "pic.jpg";
-	readimagefile(p,0,0,600,600);
+	p = "menu.jpg";
+	readimagefile(p, 0, 0, 600, 600);
 	int x2;
 	int y2;
 	while(1)
 	{
 		if(kbhit()) 
-		return // - code
+		return 3;
 		if(ismouseclick(WM_LBUTTONDOWN))
 		{
-			getmouseclick(WM_LBUTTONDOWN,x2,y2);
-			if(x2 ... y2) //coord knopok menu
+			getmouseclick(WM_LBUTTONDOWN, x2, y2);
+			if((x2 >= 195) && (x2 <= 405)) 
 			{
 				// return code ->>>>> main.cpp
 			} 
