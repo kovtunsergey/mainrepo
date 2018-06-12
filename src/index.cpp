67861bc304e4260a130d9c3620221cb682092f07
kovtunsergey <flashback2018@mail.ru>
2018-06-12 16:22:26 +0700

    dev test commit

---
+++ src/index.cpp
@@ -0,0 +1,8 @@
+bool index(int x1, int y1, int x, int y)
+{
+	if((x1 == 4) || y1 == 4)) 
+	{
+		return 0;
+	}
+	if((((x + 1) == x1) && (y1 == y)) || (((x - 1) == x1) && (y1 == y)) || (((y + 1) == y1)&&))
+}
